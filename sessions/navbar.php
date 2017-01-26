61c1bc00519850522d9c7b27d46cd60452afb10b
Sydney Coco <chnsydney@gmail.com>
2017-01-26 11:15:10 +0100

    style

--- sessions/navbar.php
+++ sessions/navbar.php
@@ -6,14 +6,14 @@ session_start();
 <html>
 <head>
   <title>Selfie Generator</title>
-<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<link rel="stylesheet" type="text/css" href="../stylesheet.css">
 
 </head>
 <body>
 
 <div class="navbar">
   <ul>
-    <li> <a href="scene.php"> <strong> Selfie Generator </strong> </a></li>
+    <li> <a href="../scene.php"> <strong> Selfie Generator </strong> </a></li>
 <!-- Navbar -->
 
 <?php
@@ -25,7 +25,7 @@ if ($_SESSION['loggued_on_user'] != "") {
     print("<li style='float:right' ><a href=\"account.php\">My Account</a></li>");
     print("<li style='float:right' ><a href=\"sessions/logout.php\">Log Out</a></li>");
 } else {
-    print("<li><a href=\"sessions/new_user.php\">Sign up</a></li><li><a href=\"sessions/login_form.php\">Login</a></li>");
+    print("<li><a href=\"./new_user.php\">Sign up</a></li><li><a href=\"./login_form.php\">Login</a></li>");
 }
 print("</div>");?>
   </ul>
--- sessions/new_user.php
+++ sessions/new_user.php
@@ -1,3 +1,8 @@
+<?php
+include ("navbar.php");
+ ?>
+ <br>
+ <div style="text-align: center;">
 <form action="create_user.php" method="post">
   Email: <input type="email" name="email" required/>
   <br />
@@ -6,3 +11,4 @@
   Mot de passe: <input type="password" name="password" required/>
   <br />
   <input type="submit" name="submit" value="OK"/></form>
+</div>
